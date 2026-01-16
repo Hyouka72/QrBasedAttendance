2a09dc3c6b638d2d5e59648475e6cc7e6cecad49
Hyouka45 <khewang212@gmail.com>
2026-01-16 12:21:17 +0545

    first commit

---
+++ src/main/java/com/Qr/Attendace/AttendaceApplication.java
@@ -0,0 +1,13 @@
+package com.Qr.Attendace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Qr/Attendace/AttendaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Qr.Attendace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
